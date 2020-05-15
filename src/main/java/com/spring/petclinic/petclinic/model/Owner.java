a2b23cf7c561ea2eaae10b2b85a6bf3e7a8c1c3d
Vipul Popli <vipul.popli@cleartrip.com>
2020-05-15 22:58:58 +0530

    added petcinic POJOs

---
+++ src/main/java/com/spring/petclinic/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.spring.petclinic.petclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/com/spring/petclinic/petclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package com.spring.petclinic.petclinic.model;
+
+import java.util.Date;
+
+public class Pet {
+    private Owner owner;
+    private Date birthDate;
+    private PetType petType;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+}
