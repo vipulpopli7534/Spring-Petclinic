9362530ae100c7b181620e45b82b94eb1cb43c67
Vipul Popli <vipul.popli@cleartrip.com>
2020-05-16 02:40:35 +0530

    multi-Moduling done

--- web/src/main/java/com/spring/petclinic/petclinic/PetClinicApplication.java
+++ web/src/main/java/com/spring/petclinic/petclinic/PetClinicApplication.java
@@ -1,5 +1,7 @@
 package com.spring.petclinic.petclinic;
 
+import com.spring.petclinic.petclinic.model.Person;
+import com.spring.petclinic.petclinic.model.Vet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,7 @@ public class PetClinicApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
+		Person person = new Person();
 	}
 
 }
